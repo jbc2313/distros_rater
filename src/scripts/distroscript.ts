6c4d0a80cace8f3616d2194096419b98f61c2d31
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-04-21 11:23:59 -0500

    added prevent default for form to keep it client side

--- src/scripts/distroscript.ts
+++ src/scripts/distroscript.ts
@@ -1,14 +1,36 @@
 
 const pageDialogue = document.getElementById("pageDialogue");
-
+const closeDialog = document.getElementById("closeDialog");
+const dialogSubmit = document.getElementById("dialogSubmit");
+let distroChosen;
+let distroRank;
 
 const rateButton = document.getElementById("RateButton");
 if(rateButton != null){
     rateButton.addEventListener("click", (ev)=>{
         if(ev.target instanceof HTMLButtonElement) 
-        console.log(ev.target.innerHTML);
+        //console.log(ev.target.innerHTML);
         if(pageDialogue instanceof HTMLDialogElement)
         pageDialogue.showModal(); 
     });
 }
+
+dialogSubmit?.addEventListener('click', (event)=>{ 
+    event.preventDefault();
+    distroChosen = document.getElementById("DistroInput");
+    if(distroChosen instanceof HTMLInputElement) 
+    console.log(distroChosen.value);
+    //place user chosen info in the table here
+
+    distroRank = document.getElementById("DistroRank");
+    if(distroRank instanceof HTMLInputElement)
+    console.log(distroRank.value);
+    //place user chosen info in the table here
+
+    if(pageDialogue instanceof HTMLDialogElement)
+    pageDialogue.close(); 
+    
+});
+
+
 
