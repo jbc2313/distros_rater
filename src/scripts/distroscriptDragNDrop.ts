ce1a8a48ed7b6457281514c7a99a081f8c5dcb8b
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-04-24 10:04:39 -0500

    working on dragndrop script
    +3

--- src/scripts/distroscriptDragNDrop.ts
+++ src/scripts/distroscriptDragNDrop.ts
@@ -1,6 +1,13 @@
-function drag(e: Event) {
-    if(e.target instanceof HTMLImageElement)
-    if(e)
-    e.dataTransfer.setData('text', e.target.id);
-
+function dragstarter(e: Event) {
+    if(e instanceof DragEvent) {
+        if(e.target instanceof HTMLElement) {
+            if(e.dataTransfer != null) {
+                e.dataTransfer.setData("text/plain", e.target.id);
+                e.dataTransfer.effectAllowed = "move";
+                e.dataTransfer.dropEffect = "move";
+            }
+        }
+    }
 };
+
+
